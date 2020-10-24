804554a82dc632d236846f0b5e168520e3462847
Maxence Marchal <maxence.marchal@gmail.com>
2020-10-24 21:04:44 +0200

    Add GET request for expenses based on user id

--- serverless/user-home/expenses.js
+++ serverless/user-home/expenses.js
@@ -9,10 +9,31 @@ const TableName = process.env.RECEIPTS_TABLE;
 
 module.exports = {
     get: async(event, context) => {
+        const userId = event.requestContext.authorizer.claims.sub;
+        const params = {
+            KeyConditionExpression: "#uid = :uid",
+            ExpressionAttributeNames:{
+                "#uid": "userId"
+            },
+            ExpressionAttributeValues: {
+                ":uid": userId
+            },
+            TableName
+        }
+        let result = {};
+
+        try {
+            result = await db.query(params).promise();
+        } catch (error) {
+            console.log("An error occurred.", error);
+            return {
+                statusCode: 500
+            }
+        }
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: "This is a test",
+                result
             })
         }
     },
