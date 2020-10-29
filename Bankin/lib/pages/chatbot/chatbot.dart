2f84fa6422395ea98cbe50b67ff91c479dd970f9
angela-boyadjian <boyadjian.angela@gmail.com>
2020-10-29 08:12:11 +0100

    feat(front): post request

--- Bankin/lib/pages/chatbot/chatbot.dart
+++ Bankin/lib/pages/chatbot/chatbot.dart
@@ -1,11 +1,9 @@
-import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
-import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:amazon_cognito_identity_dart_2/cognito.dart';
-import 'package:aws_lex_runtime_api/runtime.lex-2016-11-28.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ChatBot extends StatefulWidget {
   final CognitoCredentials user;
@@ -17,38 +15,63 @@ class ChatBot extends StatefulWidget {
 }
 
 class _ChatBotState extends State<ChatBot> {
+  final http.Client _client = http.Client();
+  final String _url =
+      "https://gwdz2qxtl2.execute-api.eu-west-2.amazonaws.com/dev/chatbot/setBudget";
+  Map<String, String> _headers;
+
   @override
   void initState() {
     super.initState();
+    _headers = {
+      'Authorization': widget.idToken.getJwtToken(),
+    };
     initChatBot();
   }
 
-  Future<void> initChatBot() async {
-    AwsSigV4Client client = AwsSigV4Client(
-      DotEnv().env['ACCESS_KEY'],
-      DotEnv().env['SECRET_KEY'],
-      'https://runtime.lex.eu-west-2.amazonaws.com',
-      region: 'eu-west-2',
-      serviceName: 'lex',
-      // defaultContentType: 'application/json; charset=utf-8',
-    );
-    final signedRequest = new SigV4Request(
-      client,
-      method: 'POST',
-      path: '/bot/BudgetBud/alias/First/user/AngelaB/text',
-      headers: new Map<String, String>.from({
-        'Content-Type': 'application/json; charset=utf-8',
-        'ACCEPT': 'application/json',
-      }),
-      body: new Map<String, dynamic>.from({"inputText": "set a budget of 60 for family"}),
-    );
+  @override
+  void dispose() {
+    _client.close();
+    super.dispose();
+  }
 
-    var response = await http.post(
-      signedRequest.url,
-      headers: signedRequest.headers,
-      body: signedRequest.body,
+  Future<void> initChatBot() async {
+    // AwsSigV4Client client = AwsSigV4Client(
+    //   DotEnv().env['ACCESS_KEY'],
+    //   DotEnv().env['SECRET_KEY'],
+    //   'https://runtime.lex.eu-west-2.amazonaws.com',
+    //   region: 'eu-west-2',
+    //   serviceName: 'lex',
+    //   // defaultContentType: 'application/json; charset=utf-8',
+    // );
+    // final signedRequest = new SigV4Request(
+    //   client,
+    //   method: 'POST',
+    //   path: '/bot/BudgetBud/alias/First/user/AngelaB/text',
+    //   headers: new Map<String, String>.from({
+    //     'Content-Type': 'application/json; charset=utf-8',
+    //     'ACCEPT': 'application/json',
+    //     'Authorisation': widget.idToken.getJwtToken(),
+    //   }),
+    //   body: new Map<String, dynamic>.from({
+    //     "name": "BudgetBud",
+    //     "alias": "First",
+    //     "inputText": "set a budget of 60 for family",
+    //     "userId": "AngelaB",
+    //   }),
+    // );
+
+    var response = await _client.post(
+      _url,
+      headers: _headers,
+      body: {
+        "name": "BudgetBud",
+        "alias": "First",
+        "inputText": "set a budget of 60 for family",
+        "userId": "AngelaB",
+      },
     );
-    print('RESPONSE ===' + response.body);
+    print('RESPONSE BODY: ' + response.body);
     // final service = LexRuntimeService(
     //     region: 'eu-west-2_kT5EeqP0M',
     //     credentials: AwsClientCredentials(
