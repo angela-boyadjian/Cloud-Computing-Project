85bb6fb9db8c148b7a4473cf5cef8c8e43d27f2f
angela-boyadjian <boyadjian.angela@gmail.com>
2020-10-29 10:38:26 +0100

    feat(front): image picker not working

--- Bankin/lib/pages/profile/profile_edit.dart
+++ Bankin/lib/pages/profile/profile_edit.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:Bankin/widgets/route_manager.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:provider/provider.dart';
 
 class ProfileEdit extends StatefulWidget {
   ProfileEdit();
@@ -8,6 +14,8 @@ class ProfileEdit extends StatefulWidget {
 }
 
 class _ProfileEditState extends State<ProfileEdit> {
+  String avatarUrl = '';
+
   AppBar _buildAppBar() {
     return AppBar(
       backgroundColor: Colors.orange,
@@ -24,21 +32,55 @@ class _ProfileEditState extends State<ProfileEdit> {
           fontWeight: FontWeight.bold,
         ),
       ),
-      actions: <Widget>[
-        IconButton(
-            icon: Icon(FontAwesomeIcons.check),
-            iconSize: 25.0,
-            onPressed: () => {}),
-      ],
       elevation: 0.0,
       centerTitle: true,
     );
+  }
+
+
+  callbackUpdateAvatar(String avatar, File file) {
+    setState(() {
+      avatarUrl = avatar;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      body: Column(
+        children: <Widget>[
+          CircleAvatar(
+              backgroundColor: Colors.grey,
+              backgroundImage: CachedNetworkImageProvider('url'),
+              radius: 75.0),
+          Positioned(
+            bottom: 1,
+            right: 0,
+            child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 5, color: Colors.white),
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: IconButton(
+                    icon: Icon(FontAwesomeIcons.edit, size: 28),
+                    color: Colors.white,
+                    onPressed: () async {
+                      var result = await Provider.of<RouteManager>(
+                              context,
+                              listen: false)
+                          .showImageEditor(context,
+                              cropStyle: CropStyle.circle,
+                              callback: callbackUpdateAvatar,
+                              path: "avatar");
+                      setState(() {
+                        avatarUrl = result.mediaUrl;
+                      });
+                    })),
+          ),
+        ],
+      ),
     );
   }
 }
--- Bankin/lib/widgets/route_manager.dart
+++ Bankin/lib/widgets/route_manager.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:image_cropper/image_cropper.dart';
+
 import 'package:Bankin/widgets/nav_bar.dart';
 import 'package:Bankin/pages/login/login.dart';
 import 'package:Bankin/pages/profile/profile_edit.dart';
+import 'package:Bankin/pages/profile/widgets/image_capture.dart';
 import 'package:Bankin/pages/login/widgets/confirming_user.dart';
 
 class RouteManager {
@@ -26,6 +29,13 @@ class RouteManager {
         MaterialPageRoute(
           builder: (context) => ProfileEdit(),
         ));
+  }
+  showImageEditor(context, {CropStyle cropStyle, callback, String path}) async {
+    return await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ImageCapture(
+                cropStyle: cropStyle, path: path)));
   }
   showConfirmingUser(context) {
     Navigator.push(
