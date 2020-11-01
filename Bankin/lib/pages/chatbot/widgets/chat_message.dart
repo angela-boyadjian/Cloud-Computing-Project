7aa3731820110db6298cb0efc1a3f43330c2a11e
angela-boyadjian <boyadjian.angela@gmail.com>
2020-11-01 01:26:40 +0100

    feat(front): add chat front

---
+++ Bankin/lib/pages/chatbot/widgets/chat_message.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'chat_bubble.dart';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: CircleAvatar(child: Text('B')),
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CustomPaint(
+              painter: ChatBubble(isOwn: false),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  text,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            CustomPaint(
+              painter: ChatBubble(color: Colors.green, isOwn: true),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  text,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: CircleAvatar(
+            child: Text(
+          this.name[0].toUpperCase(),
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+  }
+}
