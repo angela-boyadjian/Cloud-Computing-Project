ef87b97c57b2d56317c092b0109fe2ff8e66003b
Maxence Marchal <maxence.marchal@gmail.com>
2020-11-05 18:58:34 +0100

    Create lamda function to generate document db in dynamo for new user

--- serverless/account/handler.js
+++ serverless/account/handler.js
@@ -1,10 +1,45 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+AWS.config.update({region: process.env.AWS_REGION}); 
+const db = new AWS.DynamoDB.DocumentClient();
+const TableName = process.env.FINANCES_TABLE;
+
 module.exports = {
   setUpDatabase: async(event, context, callback) => {
-    /* SET UP INITIAL USER ROW WITH EMPTY DATA */
-    const userId = event.userName;
-    console.log(userId);
+    const userId = event.request.userAttributes.sub;
+
+    const Item = {
+      userId,
+      receipts: [],
+      budgets:[
+        {
+          id: uuid.v4(),
+          category: 'family',
+          amount: 0.0
+        },
+        {
+          id: uuid.v4(),
+          category: 'vacation',
+          amount: 0.0
+        },
+        {
+          id: uuid.v4(),
+          category: 'saving',
+          amount: 0.0
+        }
+      ]
+    };
+
+    try {
+      await db.put({
+        TableName,
+        Item
+      }).promise();
+    } catch (error) {
+      console.log("oh. An error occured.", error);
+    }
     callback(null, event);
   }
 }
