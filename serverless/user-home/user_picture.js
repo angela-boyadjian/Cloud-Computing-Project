44a34f16fc706ac12cced6dc623348d56fc0c83b
Fake022 <email@email.email>
2020-11-09 12:23:25 +0100

    Ajout du service upload image

--- serverless/user-home/user_picture.js
+++ serverless/user-home/user_picture.js
@@ -1,103 +1,123 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-
 let mime = require('mime-types');
-
+const Busboy = require("busboy");
 const s3 = new AWS.S3();
 
+const getContentType = (event) => {
+  const contentType = event.headers['content-type']
+  if (!contentType) {
+      return event.headers['Content-Type'];
+  }
+  return contentType;
+};
+
+const parser = (event) =>  new Promise((resolve, reject) => {
+    const busboy = new Busboy({
+      headers: {
+          'content-type': getContentType(event)
+      }
+  });
+
+  var result = {};
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      file.on('data', data => {
+          result.file = data;
+      });
+
+      file.on('end', () => {
+          result.filename = filename;
+          result.contentType = mimetype;
+      });
+  });
+
+  busboy.on('field', (fieldname, value) => {
+
+      result[fieldname] = value;
+  });
+
+  busboy.on('error', error => reject(error));
+  busboy.on('finish', () => {
+      event.body = result;
+      resolve(event);
+  });
+
+  busboy.write(event.body, event.isBase64Encoded ? 'base64' : 'binary');
+  busboy.end();
+});
+
+const uploadFile =  (buffer, event) => new Promise((resolve, reject) => {
+  const bucketName = "userbucketupload2";
+  const userId = event.requestContext.authorizer.claims.sub;
+  const contentType = getContentType(event);
+  const ext = mime.extension(contentType);
+  const fileName =  userId + `${Date.now()}` + ext;
+  const data = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: buffer,
+      ACL: 'authenticated-read',
+  };
+    s3.putObject(data, (error) => {
+              if (!error) {
+                  resolve(fileName);
+                  return fileName;
+              } else {
+                  reject(new Error('error during put'));
+              }
+          });
+  });
+
 module.exports = {
-    put: async (event) => {
+    post: async (event) => {
         if (event.body !== null && event.body !== undefined) {
-          const userId = event.requestContext.authorizer.claims.sub;
-          let fileContent = event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body;
-          let fileName = userId;
-          let contentType = event.headers['content-type'] || event.headers['Content-Type'];
-          let extension = contentType ? mime.extension(contentType) : '';
-          let fullFileName = extension ? `${fileName}.${extension}` : fileName;
-          try {
-             s3.putObject({
-              Bucket: "userbucketupload",
-              Key: fullFileName,
-              Body: fileContent,
-              Metadata: {}
-            }, function(err, data){
-              var params = {
-                AccessToken: event.headers.Authorization,
-                UserAttributes: [
-                  {
-                    Name: 'profile',
-                    Value: fullFileName
-                  }
-                ]
-              };
-              cognitoidentityserviceprovider.updateUserAttributes(params, function(err, data) {
-                if (err) console.log(err, err.stack);
-                else     console.log(data);
-              });
+            parser(event).then(event => {
+              uploadFile(event.body.file, event).then(fileName => {
+                var response = {
+                  "statusCode": 200,
+                  "body": fileName,
+                  "isBase64Encoded": false
+                };
+                console.log(response);
+                return(response);
+              }).catch((err) => {
+                var response_error = {
+                  "statusCode": 500,
+                  "body": JSON.stringify(err),
+                  "isBase64Encoded": false
+                };
+              console.log(response_error);
+              return(response_error);
+              })
             });
-            return {
-              statusCode: 200,
-              body: JSON.stringify(
-                {
-                  message: fullFileName,
-                  input: event,
-                },
-                null,
-                2
-              ),
-            };
-          } catch (err) {
-              var response_error = {
-                "statusCode": 500,
-                "body": JSON.stringify(err),
-                "isBase64Encoded": false
-              };
-              return (response_error);
-            }}  else {
-            var response_error = {
-              "statusCode": 500,
-              "body": JSON.stringify("Error : body missing in request"),
-              "isBase64Encoded": false
-            };
-            return(response_error);
           }
     },
-    get: async (event) => {
+    get: async (event, res, callback) => {
       try {
+        const tmp = JSON.parse(event.body);
+        var filename = tmp.filepath;
+        console.log(filename);
         var params = {
-          AccessToken: event.headers.Authorization
+          Bucket: "userbucketupload2", 
+          Key: filename
         };
-       const filename = cognitoidentityserviceprovider.getUser(params, function(err, data) {
-          if (err) console.log(err);
-          else {
-            return data.UserAttributes['picture'];
-          }
-        });
-        let data = s3.getObject({Bucket: "userbucketupload", Key: filename}, function(err, data) {
-          if (err) console.log(err, err.stack);
-          else return(data);
-        });
-        return {
-          statusCode: 200,
-          body: JSON.stringify(
-            {
-              message: "Success",
-              object: data,
-              input: event,
-            },
-            null,
-            2
-          ),
-        };
-      } catch (err) {
-          var response_error = {
-            "statusCode": 500,
-            "body": JSON.stringify(err),
+        s3.getObject(params, function(err, data) {
+          if (err)
+            return err;
+          let objectData = data.Body;
+          let response = {
+            "statusCode": 200,
+            "body": `${objectData}`,
             "isBase64Encoded": false
           };
-          return (response_error);
-        }
+          console.log(response);
+          return(response);
+        });
+      } catch (err) {
+          callback(err, null);
+      }
     },  
     delete :  async (event) => {
       if (event.body !== null && event.body !== undefined) {
@@ -108,22 +128,16 @@ module.exports = {
         let extension = contentType ? mime.extension(contentType) : '';
         let fullFileName = extension ? `${fileName}.${extension}` : fileName;
         try {
-          let data = await s3.putObject({
-            Bucket: "userbucketupload",
-            Key: fullFileName,
-            Body: fileContent,
-            Metadata: {}
-          }).promise();
+            await s3.putObject({
+              Bucket: "userbucketupload2",
+              Key: fullFileName,
+              Body: fileContent,
+              Metadata: {}
+            }).promise();
           return {
-            statusCode: 200,
-            body: JSON.stringify(
-              {
-                message: 'Successfully uploaded file' + fullFileName,
-                input: event,
-              },
-              null,
-              2
-            ),
+            "statusCode": 200,
+            "body": 'Successfully uploaded file' +`${fullFileName}`,
+            "isBase64Encoded": false
           };
         } catch (err) {
             var response_error = {
