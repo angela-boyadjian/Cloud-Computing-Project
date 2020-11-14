456ebb9c8867023f8d38bad906776c9992c9abeb
Fake022 <email@email.email>
2020-11-14 21:06:57 +0100

    serverless zero erreur, Fargate en cours de debuging après le deploy

--- serverless/fargate-spending-crawler/build-push-docker.sh
+++
@@ -1,5 +0,0 @@
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 843314631316.dkr.ecr.eu-west-2.amazonaws.com
-aws ecr create-repository --repository-name cloud-computing
-docker build -t cloud-computing .
-docker tag cloud-computing:latest 843314631316.dkr.ecr.eu-west-2.amazonaws.com/cloud-computing:latest
-docker push 843314631316.dkr.ecr.eu-west-2.amazonaws.com/cloud-computing:latest
